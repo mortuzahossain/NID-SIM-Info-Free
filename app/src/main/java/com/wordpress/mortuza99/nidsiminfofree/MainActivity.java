1a459655a84bf468ceb01cebe12225c0f456016b
Mortuza Hossain <mortuzahossain1997@gmail.com>
2018-08-02 00:44:51 +0600

    Programming Complete.👌👌👌👌👌

--- app/src/main/java/com/wordpress/mortuza99/nidsiminfofree/MainActivity.java
+++ app/src/main/java/com/wordpress/mortuza99/nidsiminfofree/MainActivity.java
@@ -1,5 +1,7 @@
 package com.wordpress.mortuza99.nidsiminfofree;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +9,8 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import com.muddzdev.styleabletoastlibrary.StyleableToast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,10 +31,10 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                switch (i){
+                switch (i) {
                     case 0:
                         // GP
-                        sendME("info","4949");
+                        sendME("info", "4949");
                         break;
                     case 1:
                         // Banglalink
@@ -46,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
                         break;
                     case 4:
                         // Teletalk
-                        sendME("info","1600");
+                        sendME("info", "1600");
                         break;
                 }
             }
@@ -56,10 +60,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void sendME(String message, String number) {
-        // TODO: SEND SMS
+        try {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("sms:" + number))
+                    .putExtra("sms_body", message)
+            );
+            showToast("Processing!!! Please Wait.");
+        } catch (Exception e) {
+            showToast("Something Wrong! Please Try Again Later.");
+        }
     }
 
+
     private void callME(String number) {
-        // TODO: MAKE CALL USING INTENT
+        try {
+            startActivity(new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", number, null)));
+            showToast("Processing!!! Please Wait.");
+        } catch (Exception e) {
+            showToast("Something Wrong! Please Try Again Later.");
+        }
     }
+
+    private void showToast(String message) {
+        StyleableToast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG, R.style.mytoast).show();
+    }
+
 }
